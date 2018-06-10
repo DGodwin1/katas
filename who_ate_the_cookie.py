6e36fc487c586fd701f05357dd618874140172a7
David Godwin <31069614+DGodwin1@users.noreply.github.com>
2018-06-10 18:10:10 +0100

    Adding functions to who_ate_the_cookie.py file

--- who_ate_the_cookie.py
+++ who_ate_the_cookie.py
@@ -1,1 +1,23 @@
 #https://www.codewars.com/kata/who-ate-the-cookie
+def cookie(a):
+    if isinstance(a, str):
+        return(the_actual_message("Zack"))
+
+    elif isinstance(a, int) or isinstance(a, float):
+        return(the_actual_message("Monica"))
+
+    else:
+        return(the_actual_message("the dog"))
+
+
+def the_actual_message(name_of_person):
+    return("Who ate the last cookie? It was {}!").format(name_of_person)
+  
+print(cookie("s"))
+#should return Zack
+print(cookie(10+1))
+# should return Monica
+print(cookie(-111.5))
+#should return Monica
+print(cookie(True))
+#should return dog
