f2c8795af7a8d9b55aa42037de40b4ebe5815084
David Godwin <31069614+DGodwin1@users.noreply.github.com>
2018-06-20 01:38:45 +0100

    Update feast.py

--- feast.py
+++ feast.py
@@ -1,1 +1,15 @@
+#https://www.codewars.com/kata/the-feast-of-many-beasts
+def feast(animal_name, dish_name):
+  #make into lists so we can compare list positions
+  a = list(animal_name)
+  b = list(dish_name)
+  return(a[0] == b[0]) and (a[-1] == b[-1]))
+  
+#should return true
+print(feast("great blue heron", "garlic naan"))
 
+#should return true
+print(feast("chickadee", "chocolate cake"))
+
+#should return false
+print(feast("zebra", "curry"))
